e0477019933bc35e1139ab744d6b3af6a9401909
Tanya Wanwatanakool <twanwatanakool@gmail.com>
2017-05-04 03:38:18 -0700

    Final updates.

--- QuickSort.java
+++ QuickSort.java
@@ -1,169 +1,60 @@
 import java.util.Random;
-import java.lang.Math;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class QuickSort {
-    private int[] arr;
+	private int[] arr;
 
-    //QuickSort Helper Method
-    public void sort(int[] arr, int i) {
-        this.arr = arr;
+	public QuickSort(int[] arr) {
+		this.arr = arr;
+	}
 
-        //Determine QuickSort implementation to use
-        switch(i) {
-            case 1:
-                quickSort(0, (arr.length-1), 1);
-                break;
-            case 2:
-                quickSort(0, (arr.length-1), 2);
-                break;
-            case 3:
-                quickSort(0, (arr.length-1), 3);
-                break;
-            default:
-                System.out.println("You provided an invalid integer parameter.");
-                break;
-        }
-    }
+	public void quickSort1(int left, int right) {
+		if (left < right) {
+			int pivotIndex = left;
+			int pos = partition(left, right, pivotIndex);
+			quickSort1(left, pos - 1);
+			quickSort1(pos + 1, right);
+		}
+	}
 
-    public void quickSort(int low, int high, int i) {
-        if(low < high) {
-            int pivotposition;
-            //Determine Partition implementation to use
-            switch(i) {
-                case 1:
-                    partition1(low, high);
-                    //pivotposition = partition1(low, high);
-                    // quickSort(low, ts-1, 1);
-                    // quickSort(ts+1, high, 1);
-                    break;
-                case 2:
-                    partition2(low, high);
-                    //pivotposition = partition2(low, high);
-                    // quickSort(low, pivotposition-1, 2);
-                    // quickSort(pivotposition+1, high, 2);
-                    break;
-                case 3:
-                    partition3(low, high);
-                    // pivotposition = partition3(low, high);
-                    // quickSort(low, pivotposition-1, 3);
-                    // quickSort(pivotposition+1, high, 3);
-                    break;
-                default:
-                    System.out.println("You provided an invalid integer parameter.");
-                    break;
-            }
-        }
-    }
+	public void quickSort2(int left, int right) {
+		if((left - right) <= 16) {
+			insertionSort(arr);
+		} else {
+			quickSort1(left, right);
+		}
+	}
 
-    //QuickSort Implementation 1:
-    //Use the 1st element as the pivot
-    public void partition1(int first, int last) {
-        if(first >= last) {
-            return;
+	public void quickSort3(int left, int right) {
+		if(left < right) {
+			partitionRandom(left, right);
+			int pivotIndex = left;
+			int pivotposition = partition(left, right, pivotIndex);
+			quickSort1(left, (pivotposition-1));
+			quickSort1((pivotposition+1), right);
+		}
+	}
+	
+	public int partition(int left, int right, int pivotIndex) {
+		int pivot = arr[pivotIndex];
+        swap(pivotIndex, right);
+        int pos = left;
+
+        for(int i = left; i < right; i++) {
+            if(arr[i]<= pivot) {
+                swap(i, pos);
+                pos++;
+            }   
         }
 
-        int pivot = arr[first];
-        int tb = first +1;
-        int ts = last;
+        swap(pos,right);
+        return pos;
+	}
 
-        while(tb <= ts) {
-            while(tb <= ts && arr[tb] < pivot) {
-                tb++;
-            }
-
-            while(tb <= ts && arr[ts] > pivot) {
-                ts--;
-            }
-
-            if(tb <= ts) {
-                int temp = arr[tb];
-                arr[tb] = arr[ts];
-                arr[ts] = temp;
-                tb++;
-                ts--;
-            } else {
-                break;
-            }
-        }
-        arr[first] = arr[ts];
-        arr[ts] = pivot;
-
-        if(first < ts) {
-            partition1(first, ts);
-        } 
-        else if(last > tb) {
-            partition1(tb, last);
-        }
-        //return ts;  //This is pivotposition
-    }
-
-    //QuickSort Implementation 2:
-    //If number of elements in arr is <= 16, then call Insertion Sort
-    public void partition2(int first, int last) {
-        if(first >= last) {
-            return;
-        }
-
-        if( (last - first) <= 16) {
-            int[] sortedSubArr = insertionSort(arr, first, last);
-
-            //Append insertion sorted sub-array to arr
-            for(int i=first; i < last; i++) {
-                arr[i] = sortedSubArr[i];
-            }
-        }
-
-        int pivot = arr[first];
-        int tb = first +1;
-        int ts = last;
-
-        while(tb <= ts) {
-            while(tb <= ts && arr[tb] < pivot) {
-                tb++;
-            }
-
-            while(tb <= ts && arr[ts] > pivot) {
-                ts--;
-            }
-
-            if(tb <= ts) {
-                int temp = arr[tb];
-                arr[tb] = arr[ts];
-                arr[ts] = temp;
-                tb++;
-                ts--;
-            } else {
-                break;
-            }
-        }
-        arr[first] = arr[ts];
-        arr[ts] = pivot;
-
-        if(first < ts) {
-            partition2(first, ts);
-        } 
-        else if(last > tb) {
-            partition2(tb, last);
-        }
-        //return ts;  //This is pivotposition
-    }
-
-    //QuickSort Implementation 3:
-    //If number of elements >= 16, then use random index as pivotposition
-    public void partition3(int first, int last) {
-        if(first >= last) {
-            return;
-        }
-
-        int randIndex = 0;
-
+	public void partitionRandom(int first, int last) {
         //Select random index
-        if((last-first+1) >= 16) {
+        if((last- (first+1)) > 16) {
             Random rand = new Random();
-            randIndex = first + rand.nextInt() % (last-first+1);
+            int randIndex = first + rand.nextInt() % (last-first+1);
 
             if(randIndex < 0) {
                 randIndex *= -1;
@@ -173,60 +64,31 @@ public class QuickSort {
             arr[first] = arr[randIndex];
             arr[randIndex] = temp;
         }
+	}
 
-        int pivot = arr[first];
-        int tb = first +1;
-        int ts = last;
+	public int[] insertionSort(int[] arr) {
+		for (int i=1; i < arr.length; i++) {
+			int value = arr[i];
+			int j = i-1;
 
-        while(tb <= ts) {
-            while(tb <= ts && arr[tb] < pivot) {
-                tb++;
-            }
+			while (j >= 0 && arr[j] > value) {
+				arr[j+1] = arr[j];
+				j = j-1;
+			}
 
-            while(tb <= ts && arr[ts] > pivot) {
-                ts--;
-            }
+			arr[j+1] = value;
+		}
+		return arr;
+	}
 
-            if(tb <= ts) {
-                int temp = arr[tb];
-                arr[tb] = arr[ts];
-                arr[ts] = temp;
-                tb++;
-                ts--;
-            } else {
-                break;
-            }
-        }
-        arr[first] = arr[ts];
-        arr[ts] = pivot;
-
-        if(first < ts) {
-            partition3(first, ts);
-        } 
-        else if(last > tb) {
-            partition3(tb, last);
-        }
-        //return ts;  //This is pivotposition
-    }
-
-    public static int[] insertionSort(int[] arr, int first, int last) {
-        int target;
-        int j;
-
-        for(int i=first; i < last; i++) {
-            target = arr[i];
-            j = i-1;
-
-            while(j >= 0 && target < arr[j]) {
-                arr[j+1] = arr[j];
-                j = j-1;
-            }
-            arr[j+1] = target;
-        }
-        return arr;
-    }
+    public void swap(int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t; 
+    }  
 
     public int[] getSortedArr() {
         return arr;
     }
+    
 }
--- TestCases.java
+++ TestCases.java
@@ -1,263 +1,386 @@
+import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Random;
 import java.lang.Math;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class TestCases {
-    private FileWriter fw1, fw2, fw3, fw4, fw5;
+	private static FileWriter fw1, fw2;
 
-    private final int EXPONENT = 17;
-    private final int TEST_CASES = 20;
-    private int[] unsortedArr, testArr, sorted;
+	private static final int EXPONENT = 17;
+    private static final int TEST_CASES = 20;
+    private static int[] unsortedArr, testArr, sorted;
 
-    private long start_time = 0;
-    private long end_time = 0;
+    private static long start_time = 0;
+    private static long end_time = 0;
 
-    private double totalTimeInsert = 0;
-    private double totalTimeMerge = 0;
-    private double totalTimeQuick1 = 0;
-    private double totalTimeQuick2 = 0;
-    private double totalTimeQuick3 = 0;
+    private static double totalTimeInsert = 0;
+    private static double totalTimeMerge = 0;
+    private static double totalTimeQuick1 = 0;
+    private static double totalTimeQuick2 = 0;
+    private static double totalTimeQuick3 = 0;
 
-    //Constructor - Initialize FileWriter Objects and output files
-    public TestCases() throws IOException {
-        File file1 = new File("Insertion_Output.txt");
+	public static void main(String[] args) throws IOException {
+		File file1 = new File("SortingAlgs_Output.txt");
         file1.createNewFile();
         fw1 = new FileWriter(file1);
-        fw1.write("--- INSERTION SORT ---\n\n");
-        
-        File file2 = new File("Merge_Output.txt");
-        file2.createNewFile();
-        fw2 = new FileWriter(file2);
-        fw2.write("--- MERGE SORT ---\n\n");
 
-        File file3 = new File("Quick1_Output.txt");
-        file3.createNewFile();
-        fw3 = new FileWriter(file3);
-        fw3.write("--- QUICK SORT1 ---\n\n");
+        fw1.write("TEST CASES #1: Unsorted Arrays");
+        fw1.flush();
+        newLine();
 
-        File file4 = new File("Quick2_Output.txt");
-        file4.createNewFile();
-        fw4 = new FileWriter(file4);
-        fw4.write("--- QUICK SORT2 ---\n\n");
-
-        File file5 = new File("Quick3_Output.txt");
-        file5.createNewFile();
-        fw5 = new FileWriter(file5); 
-        fw5.write("--- QUICK SORT3 ---\n\n");
-    }
-
-    public void runTestCases() throws IOException {
-
-        for(double i=1; i < EXPONENT; i++) {
+        //Run test cases on unsorted array
+		for(double i=1; i < EXPONENT; i++) {
             //Create array of size 2^1 to 2^16
             int arrSize = (int) Math.pow(2.0, i);
             unsortedArr = new int[arrSize];
 
-            writeArrSizeToFile(arrSize);
             generateRandVals(arrSize);
 
-            //TEST 1: Insertion Sort
-            for(int j=0; j < TEST_CASES; j++) {
-                testArr = copyArrVals(unsortedArr);
+            fw1.write("ARRAY SIZE - " + arrSize + "\n");
+            fw1.flush();
 
-                InsertionSort insert = new InsertionSort();
-                start_time = System.nanoTime();
-                sorted = insert.insertionSort(testArr);
-                end_time = System.nanoTime();
-
-                writeSortedArrToFile(sorted, j, 1);
-                postTestCase(1);
+            if(arrSize <= 32) {
+            	fw1.write("UNSORTED ARRAY - ");
+            	fw1.flush();
+            	writeArrToFile(unsortedArr);
+            	newLine();
             }
 
-            //TEST 2: Merge Sort
-            for(int j=0; j < TEST_CASES; j++) {
-                testArr = copyArrVals(unsortedArr);
+            testUnsortedArrays(arrSize);    
+    	}
 
-                MergeSort merge = new MergeSort();
-                start_time = System.nanoTime();
-                merge.sort(testArr);
-                end_time = System.nanoTime();
-                sorted = merge.getSortedArr();
-
-                writeSortedArrToFile(sorted, j, 2);
-                postTestCase(2);
-            }
-
-            //TEST 3: Quick Sort 1
-            for(int j=0; j < TEST_CASES; j++) {
-                testArr = copyArrVals(unsortedArr);
-
-                QuickSort quick1 = new QuickSort();
-                start_time = System.nanoTime();
-                quick1.sort(testArr, 1);
-                end_time = System.nanoTime();
-                sorted = quick1.getSortedArr();
-
-                writeSortedArrToFile(sorted, j, 3);
-                postTestCase(3);
-            }
-
-            //TEST 4: Quick Sort 2
-            for(int j=0; j < TEST_CASES; j++) {
-                testArr = copyArrVals(unsortedArr);
-
-                QuickSort quick2 = new QuickSort();
-                start_time = System.nanoTime();
-                quick2.sort(testArr, 2);
-                end_time = System.nanoTime();
-                sorted = quick2.getSortedArr();
-
-                writeSortedArrToFile(sorted, j, 4);
-                postTestCase(4);
-            }
-
-            //TEST 5: Quick Sort 3
-            for(int j=0; j < TEST_CASES; j++) {
-                testArr = copyArrVals(unsortedArr);
-
-                QuickSort quick3 = new QuickSort();
-                start_time = System.nanoTime();
-                quick3.sort(testArr, 3);
-                end_time = System.nanoTime();
-                sorted = quick3.getSortedArr();
-
-                writeSortedArrToFile(sorted, j, 5);
-                postTestCase(5);
-            }
-            calcAvgRuntimes();
-        }
-        closeFileWriters();
-    }
-
-    //Helper Method - Initalize unsortedArr with random integers
-    public void generateRandVals(int size) {
-        Random rand = new Random();
-        for(int i=0; i < size; i++) {
-           unsortedArr[i] = rand.nextInt(100) +1;
-        }
-    }
-
-    //Helper Method - Write array length to file
-    public void writeArrSizeToFile(int i) throws IOException {
-        fw1.write("Array Length - " + i + " elements\n");
+    	fw1.write("TEST CASES #2: Sorted Arrays");
         fw1.flush();
+        newLine();
 
-        fw2.write("Array Length - " + i + " elements\n");
-        fw2.flush();
+    	//Run test cases on sorted array
+    	for(double i=1; i < EXPONENT; i++) {
+            //Create array of size 2^1 to 2^16
+            int arrSize = (int) Math.pow(2.0, i);
+            unsortedArr = new int[arrSize];
 
-        fw3.write("Array Length - " + i + " elements\n");
-        fw3.flush();
+            generateRandVals(arrSize);
+            
+            //Sort Array
+            MergeSort ms = new MergeSort();
+            ms.sort(unsortedArr);
+            int[] sortedArr = ms.getSortedArr();
 
-        fw4.write("Array Length - " + i + " elements\n");
-        fw4.flush();
+            fw1.write("ARRAY SIZE - " + arrSize + "\n");
+            fw1.flush();
 
-        fw5.write("Array Length - " + i + " elements\n");
-        fw5.flush();
+            if(arrSize <= 32) {
+            	fw1.write("SORTED ARRAY - ");
+            	fw1.flush();
+            	writeArrToFile(sortedArr);
+            	newLine();
+            }
+
+            testSortedArrays(sortedArr, arrSize);
+    	}
+
+    	fw1.close();
     }
 
-    //Helper Method - Write sorting method output to file
-    public void writeSortedArrToFile(int[] sorted, int testNum, int i) throws IOException {
-        switch(i) {
-            case 1:
-                fw1.write("Test #" + testNum + ": ");
-                fw1.flush();
-                writeArrToFile(sorted, fw1);
-                break;
-            case 2:
-                fw2.write("Test #" + testNum + ": ");
-                fw2.flush();
-                writeArrToFile(sorted, fw2);
-                break;
-            case 3:
-                fw3.write("Test #" + testNum + ": ");
-                fw3.flush();
-                writeArrToFile(sorted, fw3);
-                break;
-            case 4:
-                fw4.write("Test #" + testNum + ": ");
-                fw4.flush();
-                writeArrToFile(sorted, fw4);
-                break;
-            case 5:
-                fw5.write("Test #" + testNum + ": ");
-                fw5.flush();
-                writeArrToFile(sorted, fw5);
-                break;
-            default: 
-                System.out.println("You enetered an invalid integer parameter.");
-                break;
+    //Helper Method - Run test cases on unsorted arrays
+    public static void testUnsortedArrays(int arrSize) throws IOException {
+    	//TEST 1: Insertion Sort
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(unsortedArr);
+
+            InsertionSort insert = new InsertionSort();
+            start_time = System.nanoTime();
+            sorted = insert.insertionSort(testArr);
+            end_time = System.nanoTime();
+            
+            postTestCase(sorted, 1);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
         }
-    }
 
-    //Helper Method - Clear arrays and update TotalTime values
-    public void postTestCase(int i) {
-        testArr = null;
-        sorted = null;
+        //TEST 2: Merge Sort
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(unsortedArr);
 
-        switch(i) {
-            case 1:
-                totalTimeInsert += (end_time - start_time);
-                break;
-            case 2:
-                totalTimeMerge += (end_time - start_time);
-                break;
-            case 3:
-                totalTimeQuick1 += (end_time - start_time);
-                break;
-            case 4:
-                totalTimeQuick2 += (end_time - start_time);
-                break;
-            case 5:
-                totalTimeQuick3 += (end_time - start_time);
-                break;
-            default:
-                System.out.println("You provided an invalid integer parameter.");
-                break;
+            MergeSort merge = new MergeSort();
+            start_time = System.nanoTime();
+            merge.sort(testArr);
+            end_time = System.nanoTime();
+            sorted = merge.getSortedArr();
+
+            postTestCase(sorted, 2);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
         }
+
+        //TEST 3: Quick Sort1
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(unsortedArr);
+
+            QuickSort quick1 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick1.quickSort1(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick1.getSortedArr();
+
+            postTestCase(sorted, 3);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+    	//TEST 4: Quick Sort2
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(unsortedArr);
+
+            QuickSort quick2 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick2.quickSort2(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick2.getSortedArr();
+
+            postTestCase(sorted, 4);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+    	//TEST 5: Quick Sort3
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(unsortedArr);
+
+            QuickSort quick3 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick3.quickSort3(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick3.getSortedArr();
+
+            postTestCase(sorted, 5);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+        calcAvgRuntimes();
     }
 
-    //Helper Method - Calculate and Write average runtimes to file
-    public void calcAvgRuntimes() throws IOException { 
+    //Helper Method - Run test cases on unsorted arrays
+    public static void testSortedArrays(int[] sortedArr, int arrSize) throws IOException {
+    	//TEST 1: Insertion Sort
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(sortedArr);
+
+            InsertionSort insert = new InsertionSort();
+            start_time = System.nanoTime();
+            sorted = insert.insertionSort(testArr);
+            end_time = System.nanoTime();
+            
+            postTestCase(sorted, 1);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+        //TEST 2: Merge Sort
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(sortedArr);
+
+            MergeSort merge = new MergeSort();
+            start_time = System.nanoTime();
+            merge.sort(testArr);
+            end_time = System.nanoTime();
+            sorted = merge.getSortedArr();
+
+            postTestCase(sorted, 2);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+        //TEST 3: Quick Sort1
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(sortedArr);
+
+            QuickSort quick1 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick1.quickSort1(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick1.getSortedArr();
+
+            postTestCase(sorted, 3);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+    	//TEST 4: Quick Sort2
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(sortedArr);
+
+            QuickSort quick2 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick2.quickSort2(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick2.getSortedArr();
+
+            postTestCase(sorted, 4);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+    	//TEST 5: Quick Sort3
+        for(int j=0; j < TEST_CASES; j++) {
+            testArr = copyArrVals(sortedArr);
+
+            QuickSort quick3 = new QuickSort(testArr);
+            start_time = System.nanoTime();
+            quick3.quickSort3(0, (arrSize-1));
+            end_time = System.nanoTime();
+      		sorted = quick3.getSortedArr();
+
+            postTestCase(sorted, 5);
+
+            if(j == (TEST_CASES -1) && arrSize <= 32) {
+            	newLine();
+            }
+        }
+
+        calcAvgRuntimes();
+    }
+
+    //Helper Method - Debugging
+    public static void printArray(int[]t) {
+		for (int i : t) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+	//Helper Method - Calculate and Write average runtimes to file
+    public static void calcAvgRuntimes() throws IOException { 
         double avgTimeInsert = 0;
         double avgTimeMerge = 0;
         double avgTimeQuick1 = 0;
         double avgTimeQuick2 = 0;
         double avgTimeQuick3 = 0;
 
-        avgTimeInsert = (totalTimeInsert / 15.0);
-        fw1.write("Average Runtime: " + avgTimeInsert + "\n\n");
+        fw1.write("\n--- AVERAGE RUNTIMES ---\n");
+
+        avgTimeInsert = (totalTimeInsert / 20.0);
+        fw1.write("IS AVG: " + avgTimeInsert + " ns\n");
         fw1.flush();
 
-        avgTimeMerge = (totalTimeMerge / 15.0);
-        fw2.write("Average Runtime: " + avgTimeMerge + "\n\n");
-        fw2.flush();
+        avgTimeMerge = (totalTimeMerge / 20.0);
+        fw1.write("MS AVG: " + avgTimeMerge + " ns\n");
+        fw1.flush();
 
-        avgTimeQuick1 = (totalTimeQuick1 / 15.0);
-        fw3.write("Average Runtime: " + avgTimeQuick1 + "\n\n");
-        fw3.flush();
+        avgTimeQuick1 = (totalTimeQuick1 / 20.0);
+        fw1.write("QS1 AVG: " + avgTimeQuick1 + " ns\n");
+        fw1.flush();
 
-        avgTimeQuick2 = (totalTimeQuick2 / 15.0);
-        fw4.write("Average Runtime: " + avgTimeQuick2 + "\n\n");
-        fw4.flush();
+        avgTimeQuick2 = (totalTimeQuick2 / 20.0);
+        fw1.write("QS2 AVG: " + avgTimeQuick2 + " ns\n");
+        fw1.flush();
 
-        avgTimeQuick3 = (totalTimeQuick3 / 15.0);
-        fw5.write("Average Runtime: " + avgTimeQuick3 + "\n\n");
-        fw5.flush();
+        avgTimeQuick3 = (totalTimeQuick3 / 20.0);
+        fw1.write("QS3 AVG: " + avgTimeQuick3 + " ns\n");
+        fw1.flush();
+
+        fw1.write("\n\n\n");
+        fw1.flush();
     }
 
-    //Helper Method - Closes FileWriter Objects
-    public void closeFileWriters() throws IOException {
-        fw1.close();
-        fw2.close();
-        fw3.close();
-        fw4.close();
-        fw5.close(); 
+    //Helper Method - Initalize unsortedArr with random integers
+    public static void generateRandVals(int size) {
+        Random rand = new Random();
+        for(int i=0; i < size; i++) {
+           unsortedArr[i] = rand.nextInt();
+        }
+    }
+
+    //Helper Method - Clear arrays and update TotalTime values
+    public static void postTestCase(int[] arr, int i) throws IOException {
+        switch(i) {
+        	case 1:
+        		totalTimeInsert += (end_time - start_time);
+	            if(arr.length <= 32) {
+		        	fw1.write("INSERTION SORT: ");
+		        	fw1.flush();
+		        	writeArrToFile(arr);
+	       		 }
+        		break;
+        	case 2:
+        		totalTimeMerge += (end_time - start_time);
+        		if(arr.length <= 32) {
+	        		fw1.write("MERGE SORT: ");
+	        		fw1.flush();
+	        		writeArrToFile(arr);
+	        	}
+        		break;
+        	case 3:
+        		totalTimeQuick1 += (end_time - start_time);
+        		if(arr.length <= 32) {
+	        		fw1.write("QUICK1 SORT: ");
+	        		fw1.flush();
+	        		writeArrToFile(arr);
+	        	}
+        		break;
+        	case 4: 
+        		totalTimeQuick2 += (end_time - start_time);
+        		if(arr.length <= 32) {
+	        		fw1.write("QUICK2 SORT: ");
+	        		fw1.flush();
+	        		writeArrToFile(arr);
+	        	}
+        		break;
+        	case 5:
+        		totalTimeQuick3 += (end_time - start_time);
+        		if(arr.length <= 32) {
+	        		fw1.write("QUICK3 SORT: ");
+	        		fw1.flush();
+	        		writeArrToFile(arr);
+	        	}
+        		break;
+        	default:
+        		System.out.println("Invalid, post test case");
+        		break;
+        }
+        
+        //CLEAR ARRAYS
+        testArr = null;
+        sorted = null;
+    }
+
+    //Helper Method - Writes content of array
+    public static void writeArrToFile(int[] arr) throws IOException {
+        for(int val: arr) {
+            fw1.write(val + " ");
+            fw1.flush();
+        }
+        newLine();
+    }
+
+    //Helper Method - Formats file
+    public static void newLine() throws IOException {
+    	fw1.write("\n");
+        fw1.flush();
     }
 
     //Helper Method - Copies unsorted array to testArr
-    public int[] copyArrVals(int[] arr) {
+    public static int[] copyArrVals(int[] arr) {
         testArr = new int[arr.length];
 
         for(int i=0; i < arr.length; i++) {
@@ -265,15 +388,4 @@ public class TestCases {
         }
         return testArr;
     }
-
-    //Helper Method - Writes content of array
-    public void writeArrToFile(int[] arr, FileWriter fw) throws IOException {
-        for(int val: arr) {
-            fw.write(val + " ");
-            fw.flush();
-        }
-        fw.write("\n");
-        fw.flush();
-    }
-
 }
